ce15453df5952642ae94279ddff5d066b1046bfd
YENAZIGMINA <129706758+YENAZIGMINA@users.noreply.github.com>
2023-07-04 18:23:28 +0900

    Add files via upload

--- renewal/js/main.js
+++ renewal/js/main.js
@@ -1,11 +1,3 @@
-//로딩 마우스 효과
-/* window.addEventListener('mousemove', function(e) {
-    const cursor = document.querySelector('.cursor');
-    cursor.style.top = `${e.clientY}px`;
-    cursor.style.left = `${e.clientX}px`;
-}); */
-
-
 //Nav 2차메뉴
 $('.cabin li').click(function (e) {
     e.preventDefault();
@@ -71,15 +63,21 @@ function slideShowLeft(n) {
 
 
 let rightWrap = document.querySelector('.right_wrap');
+let leftWrap = document.querySelector('.left_wrap');
+let clickButton=document.querySelector('.b_circle');
+
 rightWrap.addEventListener("click", function () {
     slidIndexLeft++;
     slideShowLeft(slidIndexLeft);
+
+    clickButton.style.left="1050px"
 });
 
-let leftWrap = document.querySelector('.left_wrap');
 leftWrap.addEventListener("click", function () {
     slidIndexRight++;
     slideShowRight(slidIndexRight);
+
+    clickButton.style.left= "700px";
 });
 
 
@@ -88,5 +86,26 @@ leftWrap.addEventListener("click", function () {
 //발레단소개 이미지 리플
 $(".int_img").ripples({
     resolution: 500, // 렌더링 값이 클수록 잔물결 효과가 느리게 전파
-    perturbance: 0.02, // 잔물경 굴절 강도. 0은 굴절 없음
+    perturbance: 0.02, // 잔물결 굴절 강도. 0은 굴절 없음
 });
+
+
+//마우스 커서
+const intText = document.querySelector('.int_text');
+
+intText.addEventListener('mousemove', function(e) {
+    const cursor = document.querySelector('.cursor');
+    const parentRect = cursor.parentNode.getBoundingClientRect();
+
+    cursor.style.display="block";
+    cursor.style.opacity="1";
+    cursor.style.top = `${e.clientY - parentRect.top}px`;
+    cursor.style.left = `${e.clientX - parentRect.left}px`;
+});
+
+intText.addEventListener('mouseleave',(e)=>{
+    const cursor = document.querySelector('.cursor');
+
+    cursor.style.display="none";
+    cursor.style.opacity="0";
+})
